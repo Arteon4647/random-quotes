eced257bfbaca216e4d4848bbf68ddeaaf69777b
Arteon4647 <artemflashsharapov@gmail.com>
2025-03-30 15:41:19 +0300

    Show favorite quotes below current quote

--- quotes.js
+++ quotes.js
@@ -2,43 +2,53 @@ const quotes = [
   {
     quote: "The only way to do great work is to love what you do",
     author: "Steve Jobs",
+    isFavorite: false,
   },
   {
     quote: "Innovation distinguishes between a leader and a follower",
     author: "Steve Jobs",
+    isFavorite: false,
   },
   {
     quote: "Your time is limited, so don't waste it living someone else's life",
     author: "Steve Jobs",
+    isFavorite: false,
   },
   {
     quote:
       "Success is not final, failure is not fatal: it is the courage to continue that counts",
     author: "Winston Churchill",
+    isFavorite: false,
   },
   {
     quote: "Do what you can, with what you have, where you are",
     author: "Theodore Roosevelt",
+    isFavorite: false,
   },
   {
     quote: "It does not matter how slowly you go as long as you do not stop",
     author: "Confucius",
+    isFavorite: false,
   },
   {
     quote: "Believe you can and you're halfway there",
     author: "Theodore Roosevelt",
+    isFavorite: false,
   },
   {
     quote: "The best way to predict the future is to create it",
     author: "Peter Drucker",
+    isFavorite: false,
   },
   {
     quote: "Happiness depends upon ourselves",
     author: "Aristotle",
+    isFavorite: false,
   },
   {
     quote: "Everything you've ever wanted is on the other side of fear",
     author: "George Addair",
+    isFavorite: false,
   },
 ];
 
