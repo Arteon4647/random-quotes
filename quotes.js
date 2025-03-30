b0ace9c63fc1d273f4e85a6901cc5397eb5fd2f4
Arteon4647 <artemflashsharapov@gmail.com>
2025-03-30 10:53:02 +0300

    Splitting app into the JS modules

---
+++ quotes.js
@@ -0,0 +1,45 @@
+const quotes = [
+  {
+    quote: "The only way to do great work is to love what you do",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "Innovation distinguishes between a leader and a follower",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts",
+    author: "Winston Churchill",
+  },
+  {
+    quote: "Do what you can, with what you have, where you are",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "It does not matter how slowly you go as long as you do not stop",
+    author: "Confucius",
+  },
+  {
+    quote: "Believe you can and you're halfway there",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "The best way to predict the future is to create it",
+    author: "Peter Drucker",
+  },
+  {
+    quote: "Happiness depends upon ourselves",
+    author: "Aristotle",
+  },
+  {
+    quote: "Everything you've ever wanted is on the other side of fear",
+    author: "George Addair",
+  },
+];
+
+export default quotes;
--- script.js
+++ script.js
@@ -1,46 +1,4 @@
-const quotes = [
-  {
-    quote: "The only way to do great work is to love what you do",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "Innovation distinguishes between a leader and a follower",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "Your time is limited, so don't waste it living someone else's life",
-    author: "Steve Jobs",
-  },
-  {
-    quote:
-      "Success is not final, failure is not fatal: it is the courage to continue that counts",
-    author: "Winston Churchill",
-  },
-  {
-    quote: "Do what you can, with what you have, where you are",
-    author: "Theodore Roosevelt",
-  },
-  {
-    quote: "It does not matter how slowly you go as long as you do not stop",
-    author: "Confucius",
-  },
-  {
-    quote: "Believe you can and you're halfway there",
-    author: "Theodore Roosevelt",
-  },
-  {
-    quote: "The best way to predict the future is to create it",
-    author: "Peter Drucker",
-  },
-  {
-    quote: "Happiness depends upon ourselves",
-    author: "Aristotle",
-  },
-  {
-    quote: "Everything you've ever wanted is on the other side of fear",
-    author: "George Addair",
-  },
-];
+import quotes from "./quotes.js";
 
 const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("quote-author");
@@ -50,7 +8,7 @@ function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
 
-  const { quote, author} = randomQuote;
+  const { quote, author } = randomQuote;
 
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = author;
