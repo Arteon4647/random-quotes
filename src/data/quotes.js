b93a30b5690ea982a2b43e1ab38398e310023ebf
Arteon4647 <artemflashsharapov@gmail.com>
2025-04-27 13:47:41 +0300

    Refactoring - create favorites and quote modules

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,52 +1,52 @@
 const quotes = [
   {
-    quote: "The only way to do great work is to love what you do",
+    text: "The only way to do great work is to love what you do",
     author: "Steve Jobs",
     isFavorite: false,
   },
   {
-    quote: "Innovation distinguishes between a leader and a follower",
+    text: "Innovation distinguishes between a leader and a follower",
     author: "Steve Jobs",
     isFavorite: false,
   },
   {
-    quote: "Your time is limited, so don't waste it living someone else's life",
+    text: "Your time is limited, so don't waste it living someone else's life",
     author: "Steve Jobs",
     isFavorite: false,
   },
   {
-    quote:
+    text:
       "Success is not final, failure is not fatal: it is the courage to continue that counts",
     author: "Winston Churchill",
     isFavorite: false,
   },
   {
-    quote: "Do what you can, with what you have, where you are",
+    text: "Do what you can, with what you have, where you are",
     author: "Theodore Roosevelt",
     isFavorite: false,
   },
   {
-    quote: "It does not matter how slowly you go as long as you do not stop",
+    text: "It does not matter how slowly you go as long as you do not stop",
     author: "Confucius",
     isFavorite: false,
   },
   {
-    quote: "Believe you can and you're halfway there",
+    text: "Believe you can and you're halfway there",
     author: "Theodore Roosevelt",
     isFavorite: false,
   },
   {
-    quote: "The best way to predict the future is to create it",
+    text: "The best way to predict the future is to create it",
     author: "Peter Drucker",
     isFavorite: false,
   },
   {
-    quote: "Happiness depends upon ourselves",
+    text: "Happiness depends upon ourselves",
     author: "Aristotle",
     isFavorite: false,
   },
   {
-    quote: "Everything you've ever wanted is on the other side of fear",
+    text: "Everything you've ever wanted is on the other side of fear",
     author: "George Addair",
     isFavorite: false,
   },
